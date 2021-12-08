740cec34460d527d6c6e072acf96cf01d7261f33
jezraelhope <jezraelhope@gmail.com>
2021-12-08 17:51:22 -0600

    added iteration to app content

--- src/AppContent.js
+++ src/AppContent.js
@@ -1,62 +1,25 @@
 import React from 'react';
-import shoes from "./hope-store-photos/shoes-lilac-ninewest-1.jpg";
-import ccblouse from "./hope-store-photos/top-blouse-casualcorner-1.jpg";
-import cardigan from "./hope-store-photos/top-cardigan-annetaylor-1.jpg";
-import satin from "./hope-store-photos/top-satin-1.jpg";
-import silkpajama from "./hope-store-photos/top-silk-augustsilk-1.jpg";
-import rlldress from "./hope-store-photos/dress-rll-2.jpg";
+import data from './data'
 
 const AppContent = () => {
-	return (
-		<div className="app-content">
-			<div className="product-boxes">
-				<img
-					className="product-photo"
-					src={shoes}
-					alt="ninewest heels"
-				/>
-				<div>Nine West Lilac Heels</div>
-				<div>$16.99</div>
-			</div>
-			<div className="product-boxes">
-				<img className="product-photo" src={ccblouse} alt="cc blouse" />
-				<div>Casual Corner Vintage Blouse</div>
-				<div>$12.99</div>
-			</div>
-			<div className="product-boxes">
-				<img
-					className="product-photo"
-					src={cardigan}
-					alt="at cardigan"
-				/>
-				<div>Anne Taylor 100% Silk Cardigan</div>
-				<div>$9.99</div>
-			</div>
-			<div className="product-boxes">
-				<img className="product-photo" src={satin} alt="satin blouse" />
-				<div>Satin Blouse</div>
-				<div>$14.99</div>
-			</div>
-			<div className="product-boxes">
-				<img
-					className="product-photo"
-					src={silkpajama}
-					alt="silk pajama"
-				/>
-				<div>August Silk Pajama Top</div>
-				<div>$9.99</div>
-			</div>
-			<div className="product-boxes">
-				<img
-					className="product-photo"
-					src={rlldress}
-					alt="Ralph Lauren Dress"
-				/>
-				<div>Ralph Lauren Silk Dress</div>
-				<div>$17.99</div>
-			</div>
-		</div>
-	);
+	return <div className="app-content">
+		{
+			data.map(item => {
+				return (
+					<div className="product-boxes">
+						<img
+							className="product-photo"
+							src={item.image}
+							alt={item.id}
+						/>
+						<div>{item.name}</div>
+						<div>${item.price}</div>
+					</div>
+				);
+			})
+		}
+	</div>
+
 };
 
 export default AppContent;
---
+++ src/data.js
@@ -0,0 +1,46 @@
+import shoes from "./hope-store-photos/shoes-lilac-ninewest-1.jpg";
+import ccblouse from "./hope-store-photos/top-blouse-casualcorner-1.jpg";
+import cardigan from "./hope-store-photos/top-cardigan-annetaylor-1.jpg";
+import satin from "./hope-store-photos/top-satin-1.jpg";
+import silkpajama from "./hope-store-photos/top-silk-augustsilk-1.jpg";
+import rlldress from "./hope-store-photos/dress-rll-2.jpg";
+
+
+export default [
+	{
+		"name": "Nine West Lilac Heels",
+		"price": "16.99",
+		"image": shoes,
+		"id": "ninewest heels"
+	},
+	{
+		"name": "Casual Corner Vintage Blouse",
+		"price": "12.99",
+		"image": ccblouse,
+		"id": "casual corner blouse"
+	},
+	{
+		"name": "Anne Taylor 100% Silk Cardigan",
+		"price": "9.99",
+		"image": cardigan,
+		"id": "atcardigan"
+	},
+	{
+		"name": "Katie Lee Satin Blouse",
+		"price": "14.99",
+		"image": satin,
+		"id": "ktsatin"
+	},
+	{
+		"name": "August Silk Pajama Top",
+		"price": "9.99",
+		"image": silkpajama,
+		"id": "aspajamatop"
+	},
+	{
+		"name": "Ralph Lauren Silk Dress",
+		"price": "17.99",
+		"image": rlldress,
+		"id": "rlldress"
+	}
+]
