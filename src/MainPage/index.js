bec9431da3fea9cf5f67e01831b2e6b32bf1a57b
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2022-12-09 11:24:00 -0600

    create main page component

---
+++ src/MainPage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const MainPage = () => {
+    return(
+        <div className="maiin-page">
+            YOOOOO!
+        </div>
+    )
+}
+
+export default MainPage;
