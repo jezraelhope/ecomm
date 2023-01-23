741756de84ce9394ce1117e223fc01060db80b92
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2023-01-23 11:17:30 -0600

    fix img alt text according to accessibility standard

--- src/MainPage/Favorites.js
+++ src/MainPage/Favorites.js
@@ -1,43 +1,58 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from 'react';
 
-import useResizeObserver from '@react-hook/resize-observer'
-import products from "../Data/productDetails";
-import Slider from "./Slider";
-
+import useResizeObserver from '@react-hook/resize-observer';
+import products from '../Data/productDetails';
+import Slider from './Slider';
 
 const Favorites = () => {
     const favoriteRef = useRef(null);
     const [isDesktopSize, setIsDesktopSize] = useState(false);
-    const favorites = products.filter(product => {
-        return product?.favorite;
-    }).map(product => {
-        return (
-            <div className="each-fade favorites">
-                <div className="slider-image-container">
-                <img src={product.image[0]} className="favorites-image"/>
+    const favorites = products
+        .filter((product) => {
+            return product?.favorite;
+        })
+        .map((product) => {
+            return (
+                <div className="each-fade favorites">
+                    <div className="slider-image-container">
+                        <img
+                            src={product.image[0]}
+                            className="favorites-image"
+                            alt=""
+                        />
+                    </div>
+                    <p>{product.name}</p>
+                    <p>{product.price}</p>
                 </div>
-                <p>{product.name}</p>
-                <p>{product.price}</p>
-            </div>
-        )
-    });
+            );
+        });
     useResizeObserver(favoriteRef, (entry) => {
-        if(entry?.contentRect?.width > 1025) {
+        if (entry?.contentRect?.width > 1025) {
             setIsDesktopSize(true);
         } else {
             setIsDesktopSize(false);
         }
-    })
+    });
     return (
         <div className="favorites-container" ref={favoriteRef}>
-            <h3 className="favorite-text-positioning">Some of Our Favorite Pieces</h3>
+            <h2 className="favorite-text-positioning">
+                Some of Our Favorite Pieces
+            </h2>
             <div className="slide-container">
-                {isDesktopSize ? <div className="favorite-desktop">{favorites}</div> : <Slider items={favorites} />}
-
+                {isDesktopSize ? (
+                    <div className="favorite-desktop">{favorites}</div>
+                ) : (
+                    <Slider items={favorites} />
+                )}
             </div>
-            <a href="/products" className="favorite-text-positioning favorite-button">Shop All</a>
+            <a
+                href="/products"
+                className="favorite-text-positioning favorite-button"
+            >
+                Shop All
+            </a>
         </div>
-    )
-}
+    );
+};
 
-export default Favorites
+export default Favorites;
