a741efb73f6eaeb6a1f0444c23ff9eccb08f19f0
jezraelhope <jezraelhope@gmail.com>
2023-08-23 13:52:20 -0500

    Fix brand link

--- src/Shared/Header/Brand.js
+++ src/Shared/Header/Brand.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+
 
 const Brand = (props) => {
     return (
-        <Link to="/" className="brand">
-            <header onClick={props.filterItems}>
+        <header onClick={props.filterItems}>
+            <a className='brand' href="https://nzm.netlify.app/">
                 <h1>NZM</h1>
-            </header>
-        </Link>
+            </a>
+        </header>
     );
 };
 
